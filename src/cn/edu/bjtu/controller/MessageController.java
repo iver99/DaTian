d684947adb312ff7011dea0a2df0decb2a3b98b5
iver99 <673850131@qq.com>
2015-06-19 20:58:05 +0800

    添加显示用户历史留言

--- src/cn/edu/bjtu/controller/MessageController.java
+++ src/cn/edu/bjtu/controller/MessageController.java
@@ -2,10 +2,15 @@ package cn.edu.bjtu.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONArray;
+import com.mysql.jdbc.Messages;
 
 import cn.edu.bjtu.service.MessageService;
 import cn.edu.bjtu.util.Constant;
@@ -15,27 +20,36 @@ public class MessageController {
 
 	@Autowired
 	MessageService messageService;
-	@RequestMapping("insertmessage")
+	
 	/**
 	  * 留言页面
 	  * @param request
 	  * @param response
 	  * @return
- 	  */
-	public String insertMessage(
-			HttpServletRequest request,HttpServletResponse response) throws Exception{
-			String clientId=(String)request.getSession().getAttribute(Constant.USER_ID);
-			/*if(clientId==null)
-			{
-				mv.setViewName("login");
-				return mv;
-			}*/
-			String content = request.getParameter("content");
-			boolean flag = true;
-			flag = messageService.insertMessage(clientId,content);
-			response.setContentType("text/html;charset=UTF-8");
-			response.getWriter().print("true");				
-				
-			return null;
-		}
+	  */
+	@RequestMapping("insertmessage")
+	public String insertMessage(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String clientId = (String) request.getSession().getAttribute(
+				Constant.USER_ID);
+		String content = request.getParameter("content");
+		boolean flag = true;
+		flag = messageService.insertMessage(clientId, content);
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().print("true");
+
+		return null;
+	}
+	/**
+	 * 获取用户所有的留言
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("")
+	@ResponseBody
+	public String getAllUserMessage(HttpSession session){
+		JSONArray jsonArray = messageService.getAllUserMessage(session);
+		
+		return jsonArray.toString();
+	}
 }
--- src/cn/edu/bjtu/service/impl/MessageServiceImpl.java
+++ src/cn/edu/bjtu/service/impl/MessageServiceImpl.java
@@ -1,17 +1,25 @@
 package cn.edu.bjtu.service.impl;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import cn.edu.bjtu.dao.FocusDao;
 import cn.edu.bjtu.dao.MessageDao;
 import cn.edu.bjtu.dao.impl.BaseDaoImpl;
 import cn.edu.bjtu.service.MessageService;
+import cn.edu.bjtu.util.Constant;
 import cn.edu.bjtu.util.IdCreator;
 import cn.edu.bjtu.vo.Message;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 @Service
 @Transactional
 public class MessageServiceImpl extends BaseDaoImpl<Message> implements MessageService{
@@ -34,4 +42,25 @@ public class MessageServiceImpl extends BaseDaoImpl<Message> implements MessageS
 		messageDao.save(message);
 		return true;
 	}
+
+	/**
+	 * 返回用户所有的留言
+	 */
+	@Override
+	public JSONArray getAllUserMessage(HttpSession session) {
+		// TODO Auto-generated method stub
+		String userId=(String)session.getAttribute(Constant.USER_ID);
+		String hql="from Message t where t.clientId=:clientId";
+		Map<String,Object> params=new HashMap<String,Object>();
+		params.put("clientId", userId);
+		List<Message> msgList=messageDao.find(hql, params);
+		JSONArray jsonArray = new JSONArray();
+		for(int i=0;i<msgList.size();i++){
+			JSONObject jsonObject=(JSONObject)JSONObject.toJSON(msgList.get(i));
+			jsonArray.add(jsonObject);
+		}
+		return null;
+	}
+	
+	
 }
